18e0972330aa1b82f67b9780e73b69f27e4ba70c
Dev Vuppala <dev.nandanv@gmail.com>
2019-04-15 20:31:22 -0400

    springboot getdata

--- src/app/springboot-test/app.springboot-test.componenet.ts
+++ src/app/springboot-test/app.springboot-test.componenet.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit } from "@angular/core";
-import { SpringBootUserService } from "./app.springboot.user.service";
+import { SpringBootUserService, Test } from "./app.springboot.user.service";
 
 @Component({
     selector:'spring-boot-test',
     template: `
         Message from service : {{message}}
+
+        <div *ngFor="let user of dataFromShreyService">
+            <div class="card">
+            <div class="card-header">
+                {{user.username}}
+            </div>
+            <div class="card-body">            
+                {{user.id}}
+                {{user.username}}
+                {{user.password}}
+            </div>
+            </div>
+        </div>
     `
 })
 export class SpringBootTestComponent implements OnInit{
     message: string;
+    dataFromShreyService: any[];
 
     constructor(private springBootService : SpringBootUserService) {
-        this.springBootService.sayHello().subscribe((message: any) => {
-            console.log(message);
-            this.message = message.name;
-        })
+        // this.springBootService.sayHello().subscribe((message: any) => {
+        //     console.log(message);
+        //     this.message = message.name;
+        // })
     }
     
     ngOnInit() {
-       
+       this.springBootService.getData().subscribe((data: Test[]) => {
+           console.log(data);
+           this.dataFromShreyService = data;
+       })
     }
 }
--- src/app/springboot-test/app.springboot.user.service.ts
+++ src/app/springboot-test/app.springboot.user.service.ts
@@ -8,13 +8,26 @@ import { Observable } from "rxjs";
 export class SpringBootUserService {
 
 
-    mySpringBootService = 'http://localhost:8080/user'
+    mySpringBootService = 'http://localhost:8080/user';
+    shreySpringBootService = 'http://192.168.2.236:8080/user/allusers'
     constructor(private http: HttpClient) {
 
     }
 
-    sayHello() : Observable<any> {
-        return this.http.get<any>(this.mySpringBootService + "/hello");
+    // sayHello() : Observable<any> {
+    //     return this.http.get<any>(this.mySpringBootService + "/hello");
+    // }
+
+    getData() : Observable<Test[]> {
+        let data =  this.http.get<Test[]>(this.shreySpringBootService);
+        console.log(data);
+        return data;
     }
 
+}
+
+export class Test {
+    id: string;
+    username: string;
+    password: string; 
 }
